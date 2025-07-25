f5b90c755035ca28c1eef1dfa646d4bcdaa9e72e
febenitez <febenitez@baufest.com>
2025-07-25 17:30:34 -0300

    Update Test 2

--- src/test/java/com/pei/taller/gestion_cursos/CourseRepositoryTest.java
+++ src/test/java/com/pei/taller/gestion_cursos/CourseRepositoryTest.java
@@ -2,6 +2,9 @@ package com.pei.taller.gestion_cursos;
 
 import com.pei.taller.gestion_cursos.model.Course;
 import com.pei.taller.gestion_cursos.repository.CourseRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -21,7 +24,7 @@ public class CourseRepositoryTest {
     void should_ReturnCourse_When_CorrectId() {
         // GIVEN
         Course expectedCourse = new Course(1L, "Fernando Lucchesi", "Introduction to Computer Science");
-       long id = 1L;
+        long id = 1L;
 
         // WHEN
         Optional<Course> actualCourse = courseRepository.findById(id);
@@ -51,5 +54,55 @@ public class CourseRepositoryTest {
 
         // THEN
         assertArrayEquals(courses.toArray(), actualCourses.toArray());
+    }
+
+    @Test
+    void should_Delete_when_IdCorrect(){
+        //GIVEN
+        Long id = 1L;
+
+        //WHEN
+        courseRepository.deleteById(id);
+        Optional<Course> actualCourse =  courseRepository.findById(1L);
+
+        //THEN
+        assertFalse(actualCourse.isPresent());
+
+    }
+
+    @Nested
+    class postTesting {
+        private String courseSetupName;
+        private String courseSetupAuthor;
+
+        @BeforeEach
+        void setup() {
+            this.courseSetupAuthor = "Fernando Lucchesi";
+            this.courseSetupName = "Introduction to Computer Science";
+            courseRepository.save(new Course(courseSetupAuthor, courseSetupName));
+        }
+
+        @AfterEach
+        void cleanSetup() {
+            List<Course> cursos = courseRepository.findAll();
+            courseRepository.deleteById(cursos.getLast().getIdCourse());
+            this.courseSetupName = "";
+            this.courseSetupAuthor = "";
+        }
+
+        @Test
+        void should_Returns_ok_When_PostCourse() {
+            //GIVEN
+            List<Course> cursos = courseRepository.findAll();
+            Course lastCourse = cursos.getLast();
+
+            //WHEN
+
+            //THEN
+            assertAll(
+                    () -> assertEquals(lastCourse.getName(), courseSetupName),
+                    () -> assertEquals(lastCourse.getAuthor(), courseSetupAuthor)
+            );
+        }
     }
 }
